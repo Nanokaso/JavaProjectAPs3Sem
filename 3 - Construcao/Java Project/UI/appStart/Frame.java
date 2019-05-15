5af4cdf45ad6aa48146d444b6d5068d513622404
Nanokaso <NanokasoGongon@users.noreply.github.com>
2019-05-15 00:50:57 -0300

    update new

--- 3 - Construcao/Java Project/UI/appStart/Frame.java
+++ 3 - Construcao/Java Project/UI/appStart/Frame.java
@@ -19,14 +19,20 @@ public class Frame {
 		frame.setSize(dim.width - 350, dim.height - 100);
 		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
 
-		
 	}
 
 	public static void ShowFrame() {
 		if (frame != null) {
-			//frame.pack();
+			// frame.pack();
 			frame.setVisible(true);
 		}
+	}
+
+	public static void SetNewSize(int w, int h) {
+		Dimension dim = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+
+		frame.setSize(w, h);
+		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
 	}
 
 }
